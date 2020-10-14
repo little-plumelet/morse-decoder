0bc939d8e6d568672a4da6e121ccc8f196613c35
Perova <perovaa.24@gmail.com>
2020-10-14 20:20:24 +0300

    init: start task morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,59 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let i = 0;
+    let arr = Array.from(expr);
+    let arrLetters = [];
+    let arrCodes = [];
+    let count = 1;
+    let str = '';
+    while (i < arr.length) {
+        str += arr[i];
+        if (count % 10 === 0) {
+          arrLetters.push(str);
+          str = '';
+        }
+        count++;
+      i++;
+    }
+    i = 0;
+    while (i < arrLetters.length) {
+      let j = 0;
+      str = '';
+      newStr = '';
+      while (j < arrLetters[i].length) {
+        str += arrLetters[i][j];
+        if (arrLetters[i][j] === '*') {
+            newStr = ' ';
+            break;
+        }
+        if (Number(str) === 10 && (j + 1) % 2 === 0) {
+          newStr += '.';
+          str = '';
+        }
+        else if (Number(str) === 11  && (j + 1) % 2 === 0) {
+          newStr += '-';
+          str = '';
+        }
+        j++;
+        }
+        arrCodes.push(newStr);
+        i++;
+    }
+    
+    i = 0;
+    let res = '';
+    while (i < arrCodes.length) {
+        if (arrCodes[i] === ' ') {
+            res += ' ';
+        }
+        else {
+            res += MORSE_TABLE[arrCodes[i]];
+        }
+        i++;
+    }
+
+    return res;
 }
 
 module.exports = {
